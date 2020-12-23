dc1d52594c9bda004022f3e7ef40b923dee9aac5
MattCW1701 <73855600+MattCW1701@users.noreply.github.com>
2020-12-22 19:27:17 -0500

    Add files via upload

---
+++ APRS2SN/Logging.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using aprsparser;
+
+namespace APRS2SN
+{
+  class Logging
+  {
+    private string sLogFileName;
+    private object oLock = new object();
+    public Logging(string sFileName)
+    {
+      sLogFileName = sFileName;
+      if (!Directory.Exists($"{Directory.GetCurrentDirectory()}\\Logs"))
+        Directory.CreateDirectory($"{Directory.GetCurrentDirectory()}\\Logs");
+    }
+
+    public void LogException(Exception ex)
+    {
+      lock(oLock)
+      {
+        List<string> lstsLines = new List<string>();
+
+        lstsLines.Add($"{DateTime.UtcNow.ToString(Constants.Utility.DATE_TIME_FORMAT + ".ffffff")}: Exception thrown:");
+        lstsLines.Add(ex.Message);
+        lstsLines.Add(String.Empty);
+        lstsLines.Add(ex.StackTrace);
+        lstsLines.Add("***");
+
+        File.AppendAllLines($"{Directory.GetCurrentDirectory()}\\Logs\\{sLogFileName}", lstsLines);
+      }            
+    }
+
+    public void LogInformation(string sInformation)
+    {
+      lock(oLock)
+      {
+        List<string> lstsLines = new List<string>();
+
+        lstsLines.Add($"{DateTime.UtcNow.ToString(Constants.Utility.DATE_TIME_FORMAT + ".ffffff")}: Information ");
+        lstsLines.Add(sInformation);
+        lstsLines.Add("***");
+
+        File.AppendAllLines($"{Directory.GetCurrentDirectory()}\\Logs\\{sLogFileName}", lstsLines);
+      }      
+    }
+
+    public void LogAPRSPacket(AprsPacket oPacket)
+    {
+      lock(oLock)
+      {
+        List<string> lstsLines = new List<string>();
+        lstsLines.Add($"{DateTime.UtcNow.ToString(Constants.Utility.DATE_TIME_FORMAT + ".ffffff")}: Received Packet ");
+        lstsLines.Add($"Callsign: {oPacket.SourceCallsign.StationCallsign}");
+        lstsLines.Add($"Time: {oPacket.TimeStamp}");
+        lstsLines.Add($"Latitude {oPacket.Position.CoordinateSet.Latitude.Value}");
+        lstsLines.Add($"Longitude: {oPacket.Position.CoordinateSet.Longitude.Value}");
+        lstsLines.Add($"Elevation: {oPacket.Position.Altitude}");
+        lstsLines.Add($"Speed: {oPacket.Position.Speed}");
+        lstsLines.Add($"Dir: {oPacket.Position.Course}");
+        //lstsLines.Add($"Status: {oPacket.InformationField}");
+        lstsLines.Add($"Type: {oPacket.DataType}");
+        lstsLines.Add($"Message: {oPacket.MessageData.MsgText}");
+        lstsLines.Add($"Comment: {oPacket.Comment}");
+        lstsLines.Add("***");
+
+        File.AppendAllLines($"{Directory.GetCurrentDirectory()}\\Logs\\{sLogFileName}", lstsLines);
+      }
+    }
+
+    public void LogHTTPResponse(HttpResponseMessage oResponse)
+    {
+      lock(oLock)
+      {
+        List<string> lstsLines = new List<string>();
+        lstsLines.Add($"{DateTime.UtcNow.ToString(Constants.Utility.DATE_TIME_FORMAT + ".ffffff")}: HTTP Response ");
+        lstsLines.Add($"Status Code: {oResponse.StatusCode}");
+        lstsLines.Add($"Request Message: {oResponse.RequestMessage}");
+        lstsLines.Add($"Response Content: {oResponse.Content}");
+        lstsLines.Add("***");
+
+        File.AppendAllLines($"{Directory.GetCurrentDirectory()}\\Logs\\{sLogFileName}", lstsLines);
+      }
+    }
+  }
+}
---
+++ APRS2SN/Settings.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace APRS2SN
+{
+  public partial class Settings : Form
+  {
+    string sSettingsFileName;
+    public Settings()
+    {
+      InitializeComponent();
+    }
+
+    public Settings(string sFileName)
+    {
+      sSettingsFileName = sFileName;
+      InitializeComponent();
+    }
+
+    private void Settings_Load(object sender, EventArgs e)
+    {
+      if(sSettingsFileName != null && !string.IsNullOrWhiteSpace(sSettingsFileName) && File.Exists(sSettingsFileName))
+      {
+        using (StreamReader srReader = File.OpenText(sSettingsFileName))
+        {
+          string sLine;
+          string[] arsSplit;
+          Regex rApplicationIDLine = new Regex(Constants.Utility.RegexStrings.APPLICATION_ID);
+          Regex rPublicIDLine = new Regex(Constants.Utility.RegexStrings.PUBLIC_ID);
+          Regex rCallsign = new Regex(Constants.Utility.RegexStrings.CALLSIGN);
+          Regex rComment = new Regex(Constants.Utility.RegexStrings.COMMENT);
+
+          while ((sLine = srReader.ReadLine()) != null)
+          {
+            arsSplit = sLine.Split(':');
+            if (rApplicationIDLine.IsMatch(sLine))
+            {
+              txtApplicationID.Text = arsSplit[1];
+            }
+            else if (rPublicIDLine.IsMatch(sLine))
+            {
+              txtPublicID.Text = arsSplit[1];
+            }
+            else if (rCallsign.IsMatch(sLine))
+            {
+              txtCallsign.Text = arsSplit[1];
+            }
+            else if(rComment.IsMatch(sLine))
+            {
+              txtCommentSearch.Text = arsSplit[1];
+            }
+          }
+        }
+      }
+    }
+
+    private void btnSave_Click(object sender, EventArgs e)
+    {
+      if(string.IsNullOrWhiteSpace(txtApplicationID.Text))
+      {
+        Validation(txtApplicationID, "Application ID cannot be empty!");
+        return;
+      }
+
+      if(string.IsNullOrWhiteSpace(txtPublicID.Text))
+      {
+        Validation(txtPublicID, "Public ID cannot be empty!");
+        return;
+      }
+
+      if(!Regex.IsMatch(txtPublicID.Text, @"\d*"))
+      {
+        Validation(txtPublicID, "Public ID must be numeric!");
+        return;
+      }
+
+      if(string.IsNullOrWhiteSpace(txtCallsign.Text))
+      {
+        Validation(txtCallsign, "Callsign cannot be empty!");
+        return;
+      }
+
+      using (StreamWriter swWriter = File.CreateText(sSettingsFileName))
+      {
+        swWriter.WriteLine($"APPLICATION_ID:{txtApplicationID.Text}");
+        swWriter.WriteLine($"PUBLIC_ID:{txtPublicID.Text}");
+        swWriter.WriteLine($"CALLSIGN:{txtCallsign.Text.ToUpper()}");
+        if(!string.IsNullOrWhiteSpace(txtCommentSearch.Text))
+          swWriter.WriteLine($"COMMENT:{txtCommentSearch.Text.ToUpper()}");        
+      }
+      this.Close();
+    }
+    
+    private void Validation(TextBox txtBox, string sMessage)
+    {
+      MessageBox.Show(sMessage, "Error!", MessageBoxButtons.OK);
+      txtBox.Focus();
+    }
+  }
+}
