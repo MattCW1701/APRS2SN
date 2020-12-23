f8983bf1d1d950b98d4183dd768e9597fce44b93
MattCW1701 <73855600+MattCW1701@users.noreply.github.com>
2020-12-22 19:32:34 -0500

    Add files via upload

---
+++ AprsParser/README.md
@@ -0,0 +1,3 @@
+# aprs-parser
+
+This library parses raw APRS packets (TNC2 format), defining only the basic elements found in all APRS packets. 
---
+++ AprsParser/TimeInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace aprsparser
+{
+  public class TimeInfo
+  {
+    public DateTime? TimeStamp { get; set; }
+    public bool IsLocalTime { get; set; }
+  }
+}
